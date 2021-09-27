dfdc5ddf6166a82cbba57f0d3858cb243072b3e6
georgegoldman <georgegoldman2014@gmail.com>
2021-09-27 17:49:05 +0100

    added another class to the structure

---
+++ src/com/genialserial/organized/Second.java
@@ -0,0 +1,9 @@
+package com.genialserial.organized;
+
+public class Second {
+    public static void main(String[] args)
+    {
+        Main myNewMainObjInSecondClass = new Main();
+        System.out.println(myNewMainObjInSecondClass.mainVar);
+    }
+}
